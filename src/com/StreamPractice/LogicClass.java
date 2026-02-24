ed39ecab249e56a7e43a7463ac577f5e1fd400f1
Rohit Palkar <rohitpalkar2080@gmail.com>
2026-02-25 00:59:40 +0530

    Stream 10 ques solved

--- src/com/StreamPractice/LogicClass.java
+++ src/com/StreamPractice/LogicClass.java
@@ -1,7 +1,10 @@
 package com.StreamPractice;
 
+import java.util.*;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Map;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class LogicClass {
@@ -29,4 +32,87 @@ public class LogicClass {
 		return answer;
 	}
 
+	public static String getOccurenceOfEachWord(String input)
+	{
+		Map<String, Long> FreqMap=Arrays.stream(input.split(" ")).collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
+	
+		String answer=FreqMap.toString();
+		return answer;
+	}
+	
+	public static String findWordwithNnumOfVowels(String input,int n)
+	{
+		String answer=Arrays.stream(input.split(" ")).distinct()
+		.filter(words->words.replaceAll("[^aeiouAEIOU]", "").length()==n)
+		.collect(Collectors.joining(" "));
+		
+		return answer;
+		
+	}
+	
+	public static List<List<Integer>> seperateList(int[] array)
+	{
+		List<List<Integer>> answermap=Arrays.stream(array).boxed().collect(Collectors.toList())
+		.stream().collect(Collectors.groupingBy(x->x%2==0,Collectors.toList()))
+		.entrySet().stream().map(x->x.getValue())
+		.collect(Collectors.toList());
+		
+		return answermap;
+		
+	}
+	
+	public static String getOccurenceOfEachChar(String input)
+	{
+	//Map<String,Long> freqMap.toString
+		String Answer= Arrays.stream(input.split(""))
+		.collect(Collectors.groupingBy(Function.identity(),Collectors.counting()))
+		.toString();
+		
+		return Answer;
+	}
+	
+	public static List<Integer> sortOnOrderCondition(int[] array,String Order)
+	{
+		List<Integer> list=Arrays.stream(array).boxed().collect(Collectors.toList());
+	
+		if(Order.equalsIgnoreCase("ASC"))
+		{
+			return list.stream().sorted().collect(Collectors.toList());
+		}
+		else if(Order.equalsIgnoreCase("DSC"))
+		{
+			return list.stream().sorted((a,b)->b-a).collect(Collectors.toList());
+		}
+		else {
+		return null;
+		}
+	}
+	
+	public static Integer getSumOfUniqueNumber(int[] array)
+	{
+		int answer=Arrays.stream(array).distinct().sum();
+		return answer;
+	}
+	
+	public static String getFirstNonrepeatedChar(String input)
+	{
+	
+		String answer=Arrays.stream(input.split(""))
+		.collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+		.entrySet().stream().filter(x->x.getValue()==1).findFirst().orElse(null).getKey();
+		
+		return answer;
+		
+	}
+	public static String getFirsRepeatedChar(String input)
+	{
+		
+		String answer=Arrays.stream(input.split(""))
+		.collect(Collectors.groupingBy(Function.identity(),LinkedHashMap::new,Collectors.counting()))
+		.entrySet().stream().filter(x->x.getValue()>1).findFirst().orElse(null).getKey();
+		
+		return answer;
+	}
+	
+	
 }
--- src/com/StreamPractice/MainApp.java
+++ src/com/StreamPractice/MainApp.java
@@ -19,7 +19,50 @@ public class MainApp {
 		String Que3="My name is Rohit palkar brother of prabhramchandra";
 		System.out.println("3.Second Longest word : "+LogicClass.getSecondLongestWord(Que3));
 
+		System.out.println("**********************************");
+		//5.Occurrence of each word
 	
+		String Que5="My name is Rohit palkar brother of prabhramchandra";
+		System.out.println("5.Occurrence of each word : "+LogicClass.getOccurenceOfEachWord(Que5));
+
+		System.out.println("**********************************");
+		//6.Find words with n number of vowels
+	
+		String Que6="My name is Rohit palkar brother of prabhramchandra";
+		int num=2;
+		System.out.println("6.Find words with n number of vowels : "+LogicClass.findWordwithNnumOfVowels(Que6,num));
+
+		System.out.println("**********************************");
+		//7.Separate odd even integer from list
+		int[] Que7= {1,2,3,4,5,9,8,7,6,5,4};
+		System.out.println("7.Separate odd even integer from list : "+LogicClass.seperateList(Que7));
+
+		System.out.println("**********************************");
+		//8.Occurrence of each character in word
+		String Que8="rohitnital";
+		System.out.println("8.Occurrence of each character in word : "+LogicClass.getOccurenceOfEachChar(Que8));
+
+		System.out.println("**********************************");
+		//9.Arrange number in ASC and DSC order
+		int[] Que9= {1,2,3,4,5,9,8,7,6,5,4};
+		String order="dSC";
+		System.out.println("9.Arrange number in ASC and DSC order : "+LogicClass.sortOnOrderCondition(Que9, order));
+
+		System.out.println("**********************************");
+		//10.Sum of unique number from Array
+		int[] Que10= {1,2,3,4,5,9,8,7,6,5,4};
+		System.out.println("10.Sum of unique number from Array : "+LogicClass.getSumOfUniqueNumber(Que10));
+
+		System.out.println("**********************************");
+		//11.First non repeating character
+		String Que11="aabcbdeff";
+		System.out.println("11.First non repeating character : "+LogicClass.getFirstNonrepeatedChar(Que11));
+
+		System.out.println("**********************************");
+		//12.First repeating character
+		String Que12="klmaabcbdeff";
+		System.out.println("12.First repeating character : "+LogicClass.getFirsRepeatedChar(Que12));
+
 	}
 
 }
